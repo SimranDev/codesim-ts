b4306dd60eafa40c0e010c34ce8fd1885a24550b
SimranDev <simran.develop@gmail.com>
2021-07-17 08:25:53 +1200

    new icons added

---
+++ src/components/skills-section/icons.ts
@@ -0,0 +1,21 @@
+import angular from "../../assets/icons/angular.png";
+import css3 from "../../assets/icons/css3.png";
+import flutter from "../../assets/icons/flutter.svg";
+import html5 from "../../assets/icons/html5.png";
+import js from "../../assets/icons/js.png";
+import materialUi from "../../assets/icons/material-ui.png";
+import react from "../../assets/icons/react.png";
+import rive from "../../assets/icons/rive.png"
+import ts from "../../assets/icons/ts.svg"
+
+export const frontEndIcons: {src:string}[] = [
+    {src: angular},
+    {src: css3},
+    {src: flutter},
+    {src: html5},
+    {src: js},
+    {src: materialUi},
+    {src: react},
+    {src: rive},
+    {src: ts},
+]
